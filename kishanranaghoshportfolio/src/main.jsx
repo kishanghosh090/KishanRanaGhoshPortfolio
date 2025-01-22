8ef30518e478d8fb757fd7b7c9373cc263243179
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-22 20:40:36 +0530

    setup completed

--- kishanranaghoshportfolio/src/main.jsx
+++ kishanranaghoshportfolio/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+//------------------- components -----------------------
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Project from "./components/Project/Project.jsx";
+import ContactMe from "./components/ContactMe/ContactMe.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
+          <Route path="contactme" element={<ContactMe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
